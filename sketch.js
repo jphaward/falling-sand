fc1bd09827d7390f4b520721502c74be8a82e8db
Jack Haward <jack.haward@bt.com>
2024-02-20 14:26:24 +0000

    feat: add limitation to falling if and below or at boundary

--- sketch.js
+++ sketch.js
@@ -5,6 +5,14 @@ let sandPit;
 let columns;
 let rows;
 
+function setup() {
+  createCanvas(800, 600);
+  colorMode(HSB, 360, 255, 255);
+  columns = width / sandSize;
+  rows = height / sandSize;
+  sandPit = createSandPit(columns, rows);
+}
+
 function createSandPit(columns, rows) {
   let sandPit = [];
   for (let column = 0; column < columns; column++) {
@@ -16,14 +24,6 @@ function createSandPit(columns, rows) {
       sandPit.push(sandColumn);
   }
   return sandPit;
-}
-
-function setup() {
-  createCanvas(800, 600);
-  colorMode(HSB, 360, 255, 255);
-  columns = width / sandSize;
-  rows = height / sandSize;
-  sandPit = createSandPit(columns, rows);
 }
 
 function draw() {
@@ -64,9 +64,25 @@ function fall() {
   let fallenSandPit = createSandPit(columns, rows);
   sandPit.forEach((sandColumn, column) => {
     sandColumn.forEach((sand, row) => {
-      fallenSandPit[column][row + 1] = sand
+      if ((sandAtXBoundary(row) || (sandBelow(column, row))) && sandPresent(column, row)) {
+        fallenSandPit[column][row] = sand
+      } else {
+        fallenSandPit[column][row + 1] = sand
+      }
     });
   });
 
   sandPit = fallenSandPit
+}
+
+function sandAtXBoundary(row) {
+  return row == rows - 1
+}
+
+function sandBelow(column, row) {
+  return sandPit[column][row + 1] != defaultColour
+}
+
+function sandPresent(column, row) {
+  return sandPit[column][row] != defaultColour
 }
