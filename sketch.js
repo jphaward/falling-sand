6ca92c25dde8affff8fe005f2e57601a827876f0
Jack Haward <jack.haward@bt.com>
2024-02-20 10:46:17 +0000

    feat: add drawing sand into sandpit

--- sketch.js
+++ sketch.js
@@ -1,12 +1,44 @@
+const sandSize = 5;
+const sandColour = 42;
+
+function createSandPit(columns, rows) {
+  let sandPit = [];
+  for (let i = 0; i < columns; i++) {
+      let column = [];
+      for (let j = 0; j < rows; j++) {
+          column.push(0);
+      }
+      sandPit.push(column);
+  }
+  return sandPit;
+}
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(800, 600);
+  colorMode(HSB, 360, 255, 255);
+  columns = width / sandSize;
+  rows = height / sandSize;
+  sandPit = createSandPit(columns, rows);
 }
 
 function draw() {
-  if (mouseIsPressed) {
-    fill(0);
-  } else {
-    fill(255);
+  background(0);
+  noStroke();
+
+  for (let column = 0; column < columns; column++) {
+    for (let row = 0; row < rows; row++) {
+      if (sandPit[column][row] > 0) {
+        fill(sandPit[column][row], 255, 255);
+        let sandX = column * sandSize;
+        let sandY = row * sandSize;
+        square(sandX, sandY, sandSize);
+      }
+    }
   }
-  ellipse(mouseX, mouseY, 80, 80);
+}
+
+function mouseDragged() {
+  let mouseColumn = floor(mouseX / sandSize);
+  let mouseRow = floor(mouseY / sandSize);
+  sandPit[mouseColumn][mouseRow] = sandColour;
 }
